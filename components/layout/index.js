c22882b1f0d1d310e91a1b15f519acd1c0c8d755
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-08-30 20:06:01 -0300

    Fixed react warning on property

--- components/layout/index.js
+++ components/layout/index.js
@@ -6,7 +6,7 @@ export default function Layout(props) {
      <Head>
         <title>MercadoData</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
       </Head>
       {props.children}
