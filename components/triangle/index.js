2e3b9f5edc634a0c2e08137abbc34ebf0e8babb5
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-08-30 20:05:46 -0300

    Decorative triangle

---
+++ components/triangle/index.js
@@ -0,0 +1,12 @@
+import  React  from 'react';
+
+export default function Triangle() {
+  return (
+    <div className="absolute bottom-0 -right-40 animate-vote">
+         <svg width="970" height="840" viewBox="0 0 970 840" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100.522 781.964L484.974 116.073L869.427 781.964H100.522Z" stroke="white" strokeWidth="116.073"/>
+            <path className="" d="M100.522 781.964L484.974 116.073L869.427 781.964H100.522Z" stroke="white" strokeWidth="116.073"/>
+        </svg>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Layout from '@/components/layout'
 import Nav from '@/components/nav'
 import Search from '@/components/search'
+import Triangle from '@/components/triangle'
 
 export default function Home() {
   return (
     <Layout>
-      <div className="h-screen w-full p-10 bg-gradient-to-b from-astronaut to-astronaut-darker">
-        <div className="container mx-auto">
+      <div className="h-screen w-full p-10 bg-gradient-to-b from-astronaut to-astronaut-darker relative overflow-hidden">
+        <div className="container mx-auto relative z-10">
           <Nav />
           <Search />
         </div>
+        <Triangle />
       </div>
     </Layout>
   )
