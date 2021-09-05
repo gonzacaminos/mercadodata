1d6ec87e2c2c5a99f25cec46952c261259290cf8
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-09-05 12:29:27 -0300

    Fixed tailwind purging base classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, 
   theme: {
     extend: {
