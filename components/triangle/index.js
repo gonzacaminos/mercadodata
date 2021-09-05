036a5c7c7521da24d90af1f6ded46739fc42c1ab
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-09-05 13:21:38 -0300

    Fixed react warning on wrong property

--- components/triangle/index.js
+++ components/triangle/index.js
@@ -5,7 +5,7 @@ export default function Triangle() {
   return (
       <div className="absolute bottom-0 -right-40">
           <svg width="970" height="840" viewBox="0 0 970 840" fill="none" xmlns="http://www.w3.org/2000/svg" className="fill-current text-astronaut-darker">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M484.974 0L0 840H969.948L484.974 0ZM484.974 232.145L201.044 723.927H768.905L484.974 232.145Z"/>
+          <path fillRule="evenodd" clip-rule="evenodd" d="M484.974 0L0 840H969.948L484.974 0ZM484.974 232.145L201.044 723.927H768.905L484.974 232.145Z"/>
           </svg>
       </div>
   )
