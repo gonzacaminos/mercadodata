43a8cdf07b3ce571090f4930de9c3ec33c4ca9fa
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-09-05 13:27:33 -0300

    Provided main background to fix dark-theme issues

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,7 +4,7 @@ import Footer from '@/components/footer'
 
 export default function Layout(props) {
   return (
-    <main>
+    <main className="bg-white">
      <Head>
         <title>MercadoData</title>
         <meta name="description" content="Obtené información sobre tus productos de MercadoLibre"/>
