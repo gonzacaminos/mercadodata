b7929463f78427cdf5bb32ec8ba947bbbcfad70e
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-09-05 12:21:19 -0300

    Simplified triangle svg

--- components/triangle/index.js
+++ components/triangle/index.js
@@ -1,12 +1,12 @@
-import  React  from 'react';
+import  React from 'react';
 
 export default function Triangle() {
+  
   return (
-    <div className="absolute bottom-0 -right-40 animate-vote">
-         <svg width="970" height="840" viewBox="0 0 970 840" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100.522 781.964L484.974 116.073L869.427 781.964H100.522Z" stroke="white" strokeWidth="116.073"/>
-            <path className="" d="M100.522 781.964L484.974 116.073L869.427 781.964H100.522Z" stroke="white" strokeWidth="116.073"/>
-        </svg>
-    </div>
+      <div className="absolute bottom-0 -right-40">
+          <svg width="970" height="840" viewBox="0 0 970 840" fill="none" xmlns="http://www.w3.org/2000/svg" className="fill-current text-astronaut-darker">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M484.974 0L0 840H969.948L484.974 0ZM484.974 232.145L201.044 723.927H768.905L484.974 232.145Z"/>
+          </svg>
+      </div>
   )
 }
