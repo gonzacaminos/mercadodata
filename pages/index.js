34675006886af24c339fd5d3c6a102dd443b9016
gonzacaminos <gonza@gonza.io>
2023-06-30 20:46:59 -0400

    Fixed Hero overflow

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Content from '@/components/content'
 export default function Home() {
   return (
     <Layout>
-      <div className="md:h-screen w-full p-5 md:p-10 bg-gradient-to-b from-astronaut to-astronaut-darker relative overflow-hidden">
+      <div className="md:min-h-screen w-full p-5 md:p-10 bg-gradient-to-b from-astronaut to-astronaut-darker relative overflow-hidden">
         <div className="container mx-auto relative z-10">
           <Nav />
           <Search />
